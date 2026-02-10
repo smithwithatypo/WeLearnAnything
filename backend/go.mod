2c518089f7d83b3b76c6f7f4e45e5ab9990887d5
smithwithatypo <smithwithatypo@gmail.com>
2026-02-10 16:22:18 -0600

    create backend server

---
+++ backend/go.mod
@@ -0,0 +1,9 @@
+module github.com/smithwithatypo/WeLearnAnything
+
+go 1.25.3
+
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/go-chi/cors v1.2.2
+	github.com/joho/godotenv v1.5.1
+)
---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	// "github.com/smithwithatypo/WeLearnAnything/internal/handlers"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	r := chi.NewRouter()
+
+	// Middleware
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+	}))
+
+	// Test route
+	r.Get("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	// Routes
+	// r.Post("/api/summarize", handlers.SummarizeJobDescription)   # TODO:  update routes
+
+	http.ListenAndServe(":8080", r)
+}
